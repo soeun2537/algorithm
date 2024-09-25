9147e1c8055f2f254ee43fac9c31287b8f2057b7
soeun2537 <soeun2537@gmail.com>
2024-09-26 00:50:13 +0900

    Add: [BOJ 11005] 진법 변환 2

---
+++ src/baekjoon/Sol11/11005.py
@@ -0,0 +1,22 @@
+line = input()
+
+n, b = line.split()
+
+dict = {
+    0: '0', 1: '1', 2: '2', 3: '3', 4: '4', 5: '5', 6: '6', 7: '7', 8: '8', 9: '9',
+    10: 'A', 11: 'B', 12: 'C', 13: 'D', 14: 'E', 15: 'F', 16: 'G', 17: 'H', 18: 'I',
+    19: 'J', 20: 'K', 21: 'L', 22: 'M', 23: 'N', 24: 'O', 25: 'P', 26: 'Q', 27: 'R',
+    28: 'S', 29: 'T', 30: 'U', 31: 'V', 32: 'W', 33: 'X', 34: 'Y', 35: 'Z'
+}
+
+num = int(n)
+quotient = int(b)
+result = []
+
+while (num >= quotient):
+    result.append(num%quotient)
+    num = num//quotient
+
+print(dict[num], end="")
+for i in range(len(result)):
+    print(dict[result[len(result) - i - 1]], end="")
