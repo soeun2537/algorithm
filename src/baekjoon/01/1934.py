67e763f39f1a89a12d26965ac5fb309dcc4033f4
soeun2537 <soeun2537@gmail.com>
2024-11-26 18:28:50 +0900

    feat: [BOJ 1934] 최소공배수

---
+++ src/baekjoon/01/1934.py
@@ -0,0 +1,15 @@
+num = int(input())
+
+for i in range(num):
+    a, b = map(int, input().split())
+    leastCommonMultiple = 1
+
+    for j in range(2, max(a, b) + 1):
+        while (a % j == 0 and b % j == 0):
+            leastCommonMultiple *= j
+            a = a // j
+            b = b // j
+
+    leastCommonMultiple *= a
+    leastCommonMultiple *= b
+    print(leastCommonMultiple)
