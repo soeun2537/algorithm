de64eba3753b3a1a652ec5c8f464146eef99acc9
soeun2537 <soeun2537@gmail.com>
2024-12-10 13:05:54 +0900

    [BOJ 2083] 럭비 클럽

---
+++ src/baekjoon/02/2083.py
@@ -0,0 +1,8 @@
+while(True):
+    name, age, weight = map(str, input().split())
+    if (name == "#" and age == "0" and weight == "0"):
+        break
+    if (int(age) > 17 or int(weight) >= 80):
+        print(name, "Senior")
+        continue
+    print(name, "Junior")
