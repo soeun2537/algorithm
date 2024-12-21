e1dacd65631aff60bc11ecf494a5c4cde04ff4c9
soeun2537 <soeun2537@gmail.com>
2024-12-21 19:08:17 +0900

    [BOJ 10808] 알파벳 개수

---
+++ src/baekjoon/10/10808.py
@@ -0,0 +1,6 @@
+alphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+word = input()
+
+for alphabet in alphabets:
+    print(word.count(alphabet), end=" ")
